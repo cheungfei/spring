e766b18b32bbb3ead982179edfa4e25b4757791f
zhangfei <zhangfei@pmlabs.com.cn>
2021-06-18 20:53:16 +0800

    add: add tmp

---
+++ src/main/java/com/cnos/springboot/entity/PersonDest.java
@@ -0,0 +1,15 @@
+package com.cnos.springboot.entity;
+
+import lombok.Data;
+
+/**
+ * @author zhangfei
+ * @version 1.0
+ * @date 2021-06-12 22:41
+ */
+@Data
+public class PersonDest {
+    private Integer id;
+    private String username;
+    private Integer age;
+}
