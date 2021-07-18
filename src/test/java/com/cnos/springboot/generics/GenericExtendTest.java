8e58a3341ba143b519c4173e306648ba91aa56f4
zhangfei <zhangfei@pmlabs.com.cn>
2021-07-18 10:32:01 +0800

    add: add generic support

--- src/test/java/com/cnos/springboot/generics/GenericExtendTest.java
+++ src/test/java/com/cnos/springboot/generics/GenericExtendTest.java
@@ -1,6 +1,5 @@
 package com.cnos.springboot.generics;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,13 +9,15 @@ import java.util.List;
  * @date 2021-06-20 17:54
  */
 public class GenericExtendTest {
+    interface Eatable {}
+    interface Palatable {}
     static class Food {}
-    static class Fruit extends Food {}
+    static class Fruit extends Food implements Eatable, Palatable {}
     static class Apple extends Fruit {}
     static class Orange extends Fruit {}
 
-    public static void main(String[] args) throws IOException {
-/*        List<? extends Fruit> fruits = new ArrayList<>();
+/*    public static void main(String[] args) throws IOException {
+        List<? extends Fruit> fruits = new ArrayList<>();
         //不能加入任何元素
 //        fruits.add(new Food());     // compile error
 //        fruits.add(new Fruit());    // compile error
@@ -36,11 +37,56 @@ public class GenericExtendTest {
         // cannot be instantiated directly
 //        fruits = new ArrayList<? extends Fruit>(); // 在java中会出现 compile error: 通配符类型无法实例化
 
-        Fruit object = fruits.get(0);    // compile success*/
+        Fruit object = fruits.get(0);    // compile success
+    }*/
+
+//    public  static  void playFruitBase(List < ? super  Fruit> list){
+//        Object obj = list.get(0);
+////        Fruit fruit = list.get(0);
+//    }
+
+
+    public static void main(String[] args) {
+        List<Apple> apples = new ArrayList<>();
+        List<Orange> oranges = new ArrayList<>();
+        List<Fruit> fruits = new ArrayList<>();
+        List<Food> foods = new ArrayList<>();
+        List<Eatable> eatableList  = new ArrayList<>();
+        List<Palatable> palatableList  = new ArrayList<>();
+
+        // extend
+        playFruitExtend(apples);
+        playFruitExtend(oranges);
+        playFruitExtend(fruits);
+//        playFruitExtend(foods);  // 编译错误
+
+
+        // super
+//        playFruitSuper(apples);  // 编译错误
+//        playFruitSuper(oranges);  // 编译错误
+        playFruitSuper(fruits);
+        playFruitSuper(foods);
+        playFruitSuper(eatableList);
+        playFruitSuper(palatableList);
     }
 
-    public  static  void playFruitBase(List < ? super  Fruit> list){
-        Object obj = list.get(0);
-//        Fruit fruit = list.get(0);
+    public static  void playFruitExtend(List < ? extends Fruit> list) {
+//        list.add(new Apple());  // 编译错误
+//        list.add(new Orange()); // 编译错误
+//        list.add(new Fruit()); // 编译错误
+        list.add(null);
     }
+
+    public  static  void playFruitSuper(List < ? super  Fruit> list) {
+        list.add(new Apple());
+        list.add(new Fruit());
+//        list.add(new Food());  // 编译错误
+
+//        Eatable eatable = new Fruit();
+//        list.add(eatable);
+
+//        Palatable palatable = new Fruit();
+//        list.add(palatable);
+    }
+
 }
---
+++ src/test/java/com/cnos/springboot/stream/DefaultableFactory.java
@@ -0,0 +1,15 @@
+package com.cnos.springboot.stream;
+
+import java.util.function.Supplier;
+
+/**
+ * @author zhangfei
+ * @version 1.0
+ * @date 2021-07-17 15:33
+ */
+public interface DefaultableFactory {
+    // Interfaces now allow static methods
+    static Defaultable create(Supplier<Defaultable> supplier ) {
+        return supplier.get();
+    }
+}
