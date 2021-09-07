9c3c144e796130566499d03b3c7e42fcd49c15b1
zhangfei <zhangfei@pmlabs.com.cn>
2021-09-07 15:31:54 +0800

    fix: optimize gc log jvm parameter

--- src/test/java/com/cnos/springboot/jvm/VerboseGarbageCollectorRunner.java
+++ src/test/java/com/cnos/springboot/jvm/VerboseGarbageCollectorRunner.java
@@ -3,14 +3,14 @@ package com.cnos.springboot.jvm;
 import java.util.HashMap;
 import java.util.Map;
 
-public class Application {
+public class VerboseGarbageCollectorRunner {
     private static Map<String, String> stringContainer = new HashMap<>();
 
     public static void main(String[] args) {
         System.out.println("Start of program!");
         String stringWithPrefix = "stringWithPrefix";
 
-        // Load Java Heap with 3 M java.lang.String instances
+        // Load Java Heap with 3 M java.lang.String instances          3000000
         for (int i = 0; i < 3000000; i++) {
             String newString = stringWithPrefix + i;
             stringContainer.put(newString, newString);
